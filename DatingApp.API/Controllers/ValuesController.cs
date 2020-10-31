e2d0c282c23e717ab074acd355eca2847879866b
Moe <moe@Moes-iMac.local>
2020-10-31 14:50:48 -0400

    Initial Commit

---
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public readonly DataContext _context;
+        public ValuesController(DataContext context)
+        {
+            this._context = context;
+
+
+        }
+        // GET api/values
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            //Async Call
+            var values = await _context.Values.ToListAsync();
+            return Ok(values);
+            /*
+            Sync Call
+            var values = _context.Values.ToList();
+            return Ok(values);
+            */
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult>  GetValue(int id)
+        {
+            //Async Call
+            var value = await _context.Values.Where(x  => x.Id == id).FirstOrDefaultAsync();
+            return Ok(value);
+            /*
+            Sync Call
+           var value = _context.Values.Where(x  => x.Id == id).FirstOrDefault();
+           return Ok(value);
+           */
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
