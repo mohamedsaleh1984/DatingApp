673d5b627d774c074c8c13850e99ba6e4659f9ed
Moe <moe@Moes-iMac.local>
2020-11-01 17:31:14 -0500

    added auth controller

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -23,6 +23,7 @@ namespace DatingApp.API.Controllers
 
             //create user
             userForRegisterDto.Username = userForRegisterDto.Username.ToLower();
+            
             if (await _repo.UserExists(userForRegisterDto.Username))
                 return BadRequest("Username already exists");
 
